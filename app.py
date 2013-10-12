209d697fd1901f63d88c175f898d0192ff1c0b5d
Chao Zhang <zhangchao6865@gmail.com>
2013-10-12 23:30:00 +0800

    Finish Login

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
 import web
-        
 urls = (
-    '/(.*)', 'hello'
+    '/', 'Home',
+    '/login/', 'Login',
+    '/logout/', 'Logout',
 )
 app = web.application(urls, globals())
 
-class hello:        
-    def GET(self, name):
-        if not name: 
-            name = 'World'
-        return 'Hello, ' + name + '!'
+session = None
+if web.config.get('_session') is None:
+    session = web.session.Session(app, 
+        web.session.DiskStore('.sessions'), 
+        initializer = {
+            'userid': -1,
+            'username': None,
+        }
+    )
+    web.config._session = session
+else:
+    session = web.config._session
+
+class Home:
+    def GET(self):
+        return 'Hello, %s!' % session.username
+
+class Login:        
+    def GET(self):
+        render = web.template.render('asset')
+        return render.login()
+
+    def POST(self):
+        i = web.input()
+        session.username = i.username
+        session.userid = 1
+        raise web.seeother("/")
+
+class Logout:
+    def GET(self):
+        pass
+
 
 if __name__ == "__main__":
     app.run()
