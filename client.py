038ffa1fea5b54b6bc85456a73abecacdf554693
Chao Zhang <zhangchao6865@gmail.com>
2013-10-22 08:17:00 +0800

    Add a fake backend

--- client.py
+++ client.py
@@ -2,53 +2,76 @@ import requests
 import web
 import mocks
 import json
-mock = True
+from simplejson import JSONDecodeError
+mock = False
+base = 'http://localhost:8081'
 
 def get(url, **kwargs):
     if mock:
         return mocks.get(url, **kwargs)
     else:
-        r = requests.get(url, **kwargs);
-        return r.status_code, web.storify(r.json())
+        r = requests.get(base + url, **kwargs)
+        try:
+            return r.status_code, web.storify(r.json())
+        except JSONDecodeError:
+            return r.status_code, {}
 
 def options(url, **kwargs):
     if mock:
         return mocks.options(url, **kwargs)
     else:
-        r = requests.options(url, **kwargs);
-        return r.status_code, web.storify(r.json())
+        r = requests.options(base + url, **kwargs)
+        try:
+            return r.status_code, web.storify(r.json())
+        except JSONDecodeError:
+            return r.status_code, {}
 
 def head(url, **kwargs):
     if mock:
         return mocks.head(url, **kwargs)
     else:
-        r = requests.head(url, **kwargs);
-        return r.status_code, web.storify(r.json())
+        r = requests.head(base + url, **kwargs)
+        try:
+            return r.status_code, web.storify(r.json())
+        except JSONDecodeError:
+            return r.status_code, {}
 
 def post(url, data={}, **kwargs):
     if mock:
         return mocks.post(url, data=json.dumps(data), **kwargs)
     else:
-        r = requests.post(url, data=json.dumps(data), **kwargs);
-        return r.status_code, web.storify(r.json())
+        r = requests.post(base + url, data=json.dumps(data), **kwargs)
+        try:
+            return r.status_code, web.storify(r.json())
+        except JSONDecodeError:
+            return r.status_code, {}
 
 def put(url, data={}, **kwargs):
     if mock:
         return mocks.put(url, data=json.dumps(data), **kwargs)
     else:
-        r = requests.put(url, data=json.dumps(data), **kwargs);
-        return r.status_code, web.storify(r.json())
+        r = requests.put(base + url, data=json.dumps(data), **kwargs)
+        try:
+            return r.status_code, web.storify(r.json())
+        except JSONDecodeError:
+            return r.status_code, {}
 
 def patch(url, data={}, **kwargs):
     if mock:
         return mocks.patch(url, data=json.dumps(data), **kwargs)
     else:
-        r = requests.patch(url, data=json.dumps(data), **kwargs);
-        return r.status_code, web.storify(r.json())
+        r = requests.patch(base + url, data=json.dumps(data), **kwargs)
+        try:
+            return r.status_code, web.storify(r.json())
+        except JSONDecodeError:
+            return r.status_code, {}
 
 def delete(url, **kwargs):
     if mock:
         return mocks.delete(url, **kwargs)
     else:
-        r = requests.delete(url, **kwargs);
-        return r.status_code, web.storify(r.json())
+        r = requests.delete(base + url, **kwargs)
+        try:
+            return r.status_code, web.storify(r.json())
+        except JSONDecodeError:
+            return r.status_code, {}
